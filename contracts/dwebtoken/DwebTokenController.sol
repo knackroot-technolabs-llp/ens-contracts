f95b351900c6a0918c602bce3a62e4cdb6cd58ed
Indraraj Chavda <indraraj9999@gmail.com>
2021-08-26 22:34:14 +0530

    DwebTokenController is now controllable

--- contracts/dwebtoken/DwebTokenController.sol
+++ contracts/dwebtoken/DwebTokenController.sol
@@ -1,10 +1,11 @@
 pragma solidity >=0.8.4;
 
 import "./DwebToken.sol";
+import "../root/Controllable.sol";
 
 // TODO: when a user owns dwebtoken first time, front end must call setApprovalForAll on dwebToken contract with operater as address(this) and approved as true with owner sign
 
-contract DwebTokenController {
+contract DwebTokenController is Controllable {
 
     DwebToken public dwebToken;
 
@@ -25,23 +26,28 @@ contract DwebTokenController {
         return dwebToken.existsToken(tokenId);
     }
 
-    function burnToken(uint256 tokenId) public {
+    function burnToken(uint256 tokenId) public onlyController {
         /* TODO: signature verification has to be done before calling burn on dweb token. 
-         *       Additional param may required for this method 
+         *       Additional param may required for this method. It can be called bycontroller
+         * TODO: how to use expiry here
          */
         dwebToken.burnToken(tokenId);
     }
 
     function mintToken(address owner, uint256 id) public {
         /* TODO: signature verification has to be done before calling mintToken on dweb token. 
-         *       Additional param may required for this method 
+         *       Additional param may required for this method.
          */
+        dwebToken.mintToken(owner, id);
+    }
+
+    function mintTokenForTLD(address owner, uint256 id) public onlyController {
         dwebToken.mintToken(owner, id);
     }
 
     function transferToken(address to, uint256 id) public {
         /* TODO: signature verification has to be done before calling transferToken on dweb token. 
-         *       Additional param may required for this method 
+         *       Additional param may required for this method. can only be done by owner. signature verification will work here
          */
         dwebToken.transferToken(to, id);
     }
