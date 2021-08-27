bbd24997f24458d3d39068c7cc9acfabfc370ad3
Indraraj Chavda <indraraj9999@gmail.com>
2021-08-27 21:19:56 +0530

    made methods external instead of public

--- contracts/dwebtoken/DwebTokenController.sol
+++ contracts/dwebtoken/DwebTokenController.sol
@@ -9,24 +9,24 @@ contract DwebTokenController is Controllable {
 
     DwebToken public dwebToken;
 
-    constructor() {
-        dwebToken = new DwebToken();
+    constructor(address _rootContract) {
+        dwebToken = new DwebToken(_rootContract);
     }
 
-    function ownerOf(uint256 tokenId) public view returns (address) {
+    function ownerOf(uint256 tokenId) external view returns (address) {
         return dwebToken.ownerOf(tokenId);
     }
 
-    function isApprovedOrOwner(address spender, uint256 tokenId) public view returns (bool) {
+    function isApprovedOrOwner(address spender, uint256 tokenId) external view returns (bool) {
         return dwebToken.isApprovedOrOwner(spender, tokenId);
     }
 
-    function existsToken(uint256 tokenId) public view returns (bool) {
+    function existsToken(uint256 tokenId) external view returns (bool) {
         // TODO: it shoud not return true for expired domain
         return dwebToken.existsToken(tokenId);
     }
 
-    function burnToken(uint256 tokenId) public onlyController {
+    function burnToken(uint256 tokenId) external onlyController {
         /* TODO: signature verification has to be done before calling burn on dweb token. 
          *       Additional param may required for this method. It can be called bycontroller
          * TODO: how to use expiry here
@@ -34,18 +34,18 @@ contract DwebTokenController is Controllable {
         dwebToken.burnToken(tokenId);
     }
 
-    function mintToken(address owner, uint256 id) public {
+    function mintToken(address owner, uint256 id) external {
         /* TODO: signature verification has to be done before calling mintToken on dweb token. 
          *       Additional param may required for this method.
          */
         dwebToken.mintToken(owner, id);
     }
 
-    function mintTokenForTLD(address owner, uint256 id) public onlyController {
+    function mintTokenForTLD(address owner, uint256 id) external onlyController {
         dwebToken.mintToken(owner, id);
     }
 
-    function transferToken(address to, uint256 id) public {
+    function transferToken(address to, uint256 id) external {
         /* TODO: signature verification has to be done before calling transferToken on dweb token. 
          *       Additional param may required for this method. can only be done by owner. signature verification will work here
          */
