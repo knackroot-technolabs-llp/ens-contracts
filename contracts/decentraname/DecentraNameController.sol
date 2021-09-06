cf7c5cc6da29913e361065af9219a0c2357797c9
Indraraj Chavda <indraraj9999@gmail.com>
2021-09-06 18:07:03 +0530

    fixed todos

--- contracts/decentraname/DecentraNameController.sol
+++ contracts/decentraname/DecentraNameController.sol
@@ -22,33 +22,26 @@ contract DecentraNameController is IDecentraNameController, Controllable {
         return decentraName.isApprovedOrOwner(spender, tokenId);
     }
 
+    // This method only returns if token alredy mint or not. to know if token is valid and not expired use root.available
     function existsToken(uint256 tokenId) external virtual override view returns (bool) {
-        // TODO: it shoud not return true for expired domain
         return decentraName.existsToken(tokenId);
     }
 
     function burnToken(uint256 tokenId) external virtual override onlyController {
         /* TODO: signature verification has to be done before calling burn on dweb token. 
          *       Additional param may required for this method. It can be called bycontroller
-         * TODO-review: should we use expiry here
+         * TODO: should we use expiry here
          */
         decentraName.burnToken(tokenId);
     }
 
-    // TODO-review : we do not need sign verification here because it can only be called by controller and parent owner
     function mintToken(address owner, uint256 id) external virtual override onlyController {
-        decentraName.mintToken(owner, id);
-    }
-
-    function mintTokenForTLD(address owner, uint256 id) external virtual override onlyController {
         decentraName.mintToken(owner, id);
     }
 
     // sender should be the one who owns token id
     // this contract must be approved for sender address
-    // TODO-review 
     function transferToken(address transferTo, uint256 id, uint8 v, bytes32 r, bytes32 s, address sender) external virtual override onlyController {
-        // TODO-review : is it safe to use chaindId()
         uint256 chainId;
         assembly {
             chainId := chainid()
