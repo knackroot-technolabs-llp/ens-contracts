c89a7177924f602f2d0be237126f4afcd61e87e2
Indraraj Chavda <indraraj9999@gmail.com>
2021-09-12 18:33:21 +0530

    testing with dweb payment and other changes integration

--- manualtest/index.js
+++ manualtest/index.js
@@ -1,26 +1,33 @@
 const ethers = require("ethers");
 const namehash = require('eth-ens-namehash');
 const sha3 = require('js-sha3').keccak_256
+const crypto = require('crypto');
+
+const ZERO_HASH = '0x0000000000000000000000000000000000000000000000000000000000000000';
 
 const decentraNameControllerABI = require('../build/contracts/DecentraNameController.json');
-const dummyOracleABI = require('../build/contracts/DummyOracle.json');
+//const dummyOracleABI = require('../build/contracts/DummyOracle.json');
+const priceEstimatorABI = require('../build/contracts/PriceEstimator.json');
 const stablePriceOracleABI = require('../build/contracts/StablePriceOracle.json');
 const ensRegistryABI = require('../build/contracts/ENSRegistry.json');
 const rootABI = require('../build/contracts/Root.json');
+const decentraWebTokenABI = require('../build/contracts/DecentraWebToken.json');
 const rootRegistrarControllerABI = require('../build/contracts/RootRegistrarController.json');
 
 const decentraNameABI = require('../build/contracts/DecentraName.json');
 
+const jsonPath = "../deployments/rinkeby/";
 
-const decentraNameAddr = '0xaD95C4A37DAF2a51d31251eC8e82C48F602Ae26D';
+const decentraNameAddr = '0xC4DaCE03731D2A9Eb795aC980cD0831dDB4332Eb';
 
-const decentraNameControllerAddr = '0x53213ecAB90Bc6e32D83E405910aB27F23635fEE';
-const dummyOracleAddr = '0xac98ae75daa00ddc1996ad3a87135c19c346ce81';
-const stablePriceOracleAddr = '0x86E8f9363e799D8CCF21c92dc2cdaBAe399134F3';
-const ensRegistryAddr = '0x61E3Ee6374AE0d4c04168d666a5dAC7bCFBd57f0';
-const rootAddr = '0x39A614eDF20f573D965A1d058545CB4074E556C4';
-const rootRegistrarControllerAddr = '0x1C3F7745244E67619c27175cb21C58d195656771';
-
+const decentraNameControllerAddr = JSON.parse(JSON.stringify(require(jsonPath + "DecentraNameController.json")))['address'];
+//const dummyOracleAddr = '0xac98ae75daa00ddc1996ad3a87135c19c346ce81';
+const priceEstimatorAddr = JSON.parse(JSON.stringify(require(jsonPath + "StablePriceOracle.json")))['args'][0];
+const stablePriceOracleAddr = JSON.parse(JSON.stringify(require(jsonPath + "StablePriceOracle.json")))['address'];
+const ensRegistryAddr = JSON.parse(JSON.stringify(require(jsonPath + "ENSRegistry.json")))['address'];
+const rootAddr = JSON.parse(JSON.stringify(require(jsonPath + "Root.json")))['address'];
+const decentraWebTokenAddr = JSON.parse(JSON.stringify(require(jsonPath + "RootRegistrarController.json")))['args'][2];
+const rootRegistrarControllerAddr = JSON.parse(JSON.stringify(require(jsonPath + "RootRegistrarController.json")))['address'];
 
 
 
@@ -28,50 +35,77 @@ const rootRegistrarControllerAddr = '0x1C3F7745244E67619c27175cb21C58d195656771'
 const RPCUrl = 'https://rinkeby.infura.io/v3/a179f100768a423fa0eebdcbc984b61b';
 
 
+const deployer = "0x4CeBBdbBdFe8A1BB3F62A75B5fe9ebaE5D105f8F";
+const deployerpri = "253dd8a564bb1525bbcf322346d0524c4aaebe878b582acbf92e386177020c8c";
 const acc2 = "0x71504cbCD9E376C2f6CF791C91349B45c387Fa88"
 const acc2pri = "fd310b1dacfad5ccdb8274c69cefde7b11fa9e9e97caec4edf6d6b2bc51ce414";
-const acc3 = "0x3b242112099A4abf3Dc8aa79F669cb86FAfDCc27";
-const acc3pri = "89c66c890ba3c20eac0177c2a90b8dd34b634254681cbd99ab02b51e0deefab7";
+const acc3 = "0x654B39F5a9fC17340eE711B0C7fc0423108251E7";
+const acc3pri = "c7313b9b823b55bbff5e70d425aab815ab04ad7e9d9227ae9cf41e8b256d5aba";
 const acc4 = "0xf91eF62a17E9A3669c20088FB74E918Ecb01DAB1";
 // const acc4pri = "39d9f533e3045cdc3a0ac8d3a117efa9b35a8b9ae9634bba4d798cada7fc400f";
 
 
 const provider = new ethers.providers.JsonRpcProvider(RPCUrl);
+const deployerWallet = new ethers.Wallet(deployerpri, provider);
 const wallet2 = new ethers.Wallet(acc2pri, provider);
 const wallet3 = new ethers.Wallet(acc3pri, provider);
 
 const decentraNameControllerContract = new ethers.Contract(decentraNameControllerAddr, JSON.parse(JSON.stringify(decentraNameControllerABI)), provider);
-const dummyOracleContract = new ethers.Contract(dummyOracleAddr, JSON.parse(JSON.stringify(dummyOracleABI)), provider);
+//const dummyOracleContract = new ethers.Contract(dummyOracleAddr, JSON.parse(JSON.stringify(dummyOracleABI)), provider);
+const priceEstimatorContract = new ethers.Contract(priceEstimatorAddr, JSON.parse(JSON.stringify(priceEstimatorABI)), provider);
 const stablePriceOracleContract = new ethers.Contract(stablePriceOracleAddr, JSON.parse(JSON.stringify(stablePriceOracleABI)), provider);
 const ensRegistryContract = new ethers.Contract(ensRegistryAddr, JSON.parse(JSON.stringify(ensRegistryABI)), provider);
 const rootContract = new ethers.Contract(rootAddr, JSON.parse(JSON.stringify(rootABI)), provider);
+const decentraWebTokenContract = new ethers.Contract(decentraWebTokenAddr, JSON.parse(JSON.stringify(decentraWebTokenABI)), provider);
 const rootRegistrarControllerContract = new ethers.Contract(rootRegistrarControllerAddr, JSON.parse(JSON.stringify(rootRegistrarControllerABI)), provider);
 var decentraNameContract = new ethers.Contract(decentraNameAddr, JSON.parse(JSON.stringify(decentraNameABI)), provider);
 
 
-async function makeCommitment(name, owner, secret='') {
+async function makeCommitment(name, owner, secret, r, s, v) {
     
     const commitment = await rootRegistrarControllerContract.connect(wallet2).makeCommitment(name, owner, secret);
-    await rootRegistrarControllerContract.connect(wallet2).commit(commitment);
+    await rootRegistrarControllerContract.connect(wallet2).commit(commitment, r, s, v);
 }
 
-async function commit(name, owner, secret) {
-    await makeCommitment(name, owner, secret);
+async function commit(name, owner, secret, r, s, v) {
+    await makeCommitment(name, owner, secret, r, s, v);
 }
 
 async function register(domainName, owner, duration, secret) {
-    await commit(domainName, owner, secret);
+    const isDWEB = true;
+    await commit(domainName, owner, secret, '0x00', ZERO_HASH, ZERO_HASH);
 
-    console.log('Waiting for 7 sec');
-    await new Promise(r => setTimeout(r, 7000));
-
-    // 1 min wait
-    let price = await rootRegistrarControllerContract.connect(wallet2).rentPrice(domainName, duration);
+    let price = await rootRegistrarControllerContract.connect(wallet2).rentPrice(domainName, duration, isDWEB);
+    // add 10% buffer
     price = price.mul(110).div(100);
 
-    console.log(`calculated price: ${ethers.utils.formatUnits(price, 'ether')}`);
-    const gasLimit = ethers.utils.parseUnits('2000000', 'wei');
-    await rootRegistrarControllerContract.connect(wallet2).register(domainName, owner, duration, secret, {value: price, gasLimit});
+    if(isDWEB) {
+        console.log(`calculated dweb price: ${price /* / 1000000000000000000*/}`);
+        // set approve
+        const currentApprovedAmount = await decentraWebTokenContract.allowance(owner, rootRegistrarControllerAddr);
+        console.log(`Approved amount ${currentApprovedAmount}`);
+
+        // TODO-review
+        if(price > currentApprovedAmount) {
+            console.log(`requesting approval for ${price} dweb`);
+            await decentraWebTokenContract.connect(wallet2).approve(rootRegistrarControllerAddr, (price).toString());
+        }
+
+        console.log('Waiting for 60 sec');
+        await new Promise(r => setTimeout(r, 60000));
+
+        // register
+        const gasLimit = ethers.utils.parseUnits('2000000', 'wei');
+        await rootRegistrarControllerContract.connect(wallet2).register(domainName, owner, duration, secret, isDWEB, price.toString(), {value: '0x0', gasLimit});
+    } else {
+        console.log('Waiting for 60 sec');
+        await new Promise(r => setTimeout(r, 60000));
+        
+        console.log(`calculated eth price: ${ethers.utils.formatUnits(price, 'ether')}`);
+        const gasLimit = ethers.utils.parseUnits('2000000', 'wei');
+        await rootRegistrarControllerContract.connect(wallet2).register(domainName, owner, duration, secret, isDWEB, price, {value: price, gasLimit});
+    }
+    
 }
 
 async function getOwner(domainName) {
@@ -90,12 +124,11 @@ async function transferDomain(node, owner) {
 }
 
 async function transferSubDomain(parent, label, owner) {
-    await decentraNameContract.connect(wallet3).setApprovalForAll(decentraNameControllerAddr, true);
+    await decentraNameContract.connect(wallet2).setApprovalForAll(decentraNameControllerAddr, true);
     await ensRegistryContract.connect(wallet2).setSubnodeOwner(namehash.hash(parent), '0x' + sha3(namehash.normalize(label)), owner);
 }
 
 async function main () {
-    
 
     //let wallet1 = new ethers.Wallet(acc1);
     // let signedMsg = await wallet.signMessage(binaryData)
@@ -106,22 +139,40 @@ async function main () {
 
     // console.log(`Is domain exist: ${await decentraNameContract.existsToken(namehash.hash('abc'))}`);
     
+    const tld = 'abi'
+
     // Register domain
-    await register('abc', acc2, 31556926, '0x9ffc3ebd3f201e8eecd71b2833365ad37a2b6a74bad78f666724f9082fdd8a10');
+    await register(tld, acc2, 31556926, '0x' + crypto.randomBytes(32).toString('hex'));
 
-    // const subdomain = 'acc9'
+    // console.log(`Owner is ${await getOwner(`${tld}`)}`);
+    // transfer root domain
+    // await transferDomain(`abe`, acc3);
+
+    // console.log(`Owner is ${await getOwner(`abe`)}`);
+
+    // const subdomain = 'sub1'
     // // create subdomain acc2.abc
-    // await createSubDomain('abc', subdomain, acc2);
-    // console.log(`Owner is ${await getOwner(`${subdomain}.abc`)}`);
+    // await createSubDomain(tld, subdomain, acc2);
+    // console.log(`Owner is ${await getOwner(`${subdomain}.${tld}`)}`);
 
-    // // Transfer domain acc2.abc
-    // await transferDomain(`${subdomain}.abc`, acc3);
-
-    // console.log(`Owner is ${await getOwner(`${subdomain}.abc`)}`);
 
     // // Transfer subdomain
-    // await transferSubDomain('abc', subdomain, acc4);
-    // console.log(`Owner is ${await getOwner(`${subdomain}.abc`)}`);
+    // await transferSubDomain(tld, subdomain, acc3);
+    // console.log(`Owner is ${await getOwner(`${subdomain}.${tld}`)}`);
+
+    // // Transfer domain acc2.abc
+    // await transferDomain(`${subdomain}.${tld}`, acc3);
+
+    // console.log(`Owner is ${await getOwner(`${subdomain}.${tld}`)}`);
+
+    // set domain price
+    // const prices = [5000_000, 1200_000, 40_000];
+    // await stablePriceOracleContract.connect(deployerWallet).setPrices(prices);
+    // console.log(await stablePriceOracleContract.rentPrices(0));
+    // console.log(await stablePriceOracleContract.rentPrices(1));
+    // console.log(await stablePriceOracleContract.rentPrices(2));
+
+    // console.log(`uniswapV2Pair addr: ${await decentraWebTokenContract.uniswapV2Pair()}`);
   }
   
   main()
